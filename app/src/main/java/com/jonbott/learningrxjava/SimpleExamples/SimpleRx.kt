340386dbaf2f90b483e11b9006a7a399fecd16e7
inec <luyege+github@gmail.com>
2018-07-29 23:10:55 -0500

    nnn

--- app/src/main/java/com/jonbott/learningrxjava/SimpleExamples/SimpleRx.kt
+++ app/src/main/java/com/jonbott/learningrxjava/SimpleExamples/SimpleRx.kt
@@ -1,5 +1,15 @@
 package com.jonbott.learningrxjava.SimpleExamples
 
-object SimpleRx{
+import io.reactivex.disposables.CompositeDisposable
+import com.jakewharton.rxrelay2.BehaviorRelay
+import kotlinx.coroutines.experimental.defer
 
+object SimpleRx{
+    var bag=CompositeDisposable()
+
+    fun simpleValues(){
+        println("~~~~~~~SimipleValue~~~~~")
+
+        val someInfo= BehaviorRelay.createDefault("1")
+    }
 }
